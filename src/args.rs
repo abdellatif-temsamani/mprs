780aad1c3608e6e8863da0c59c54859d1518d02d
abdellatif-dev <abdellatif.devlog@gmail.com>
2021-09-09 04:09:12 +0100

    help menu

--- src/args.rs
+++ src/args.rs
@@ -7,27 +7,46 @@ pub fn argv_init(mut mpd_client: Client<TcpStream>) {
     let argv: env::Args = env::args();
 
     for arg in argv {
-        match &arg as &str {
+        match &arg.to_lowercase() as &str {
             "pause" => {
                 mpd_client.pause(true);
                 println!("paused");
             }
 
             "play" => {
-                println!("playing next; {:?}", mpd_client.stats());
                 mpd_client.play();
                 println!("playing");
             }
 
             "next" => {
                 mpd_client.next();
+                println!("goint to the next");
             }
 
             "prev" => {
                 mpd_client.prev();
-                println!("playing");
+                println!("goint to the prev");
             }
+
+            "stats" => println!("{:?}", mpd_client.stats().ok().unwrap()),
+
+            "-h" | "--help" | "help" => help_menu(),
+
             _ => continue,
         }
     }
+}
+
+fn help_menu() {
+    println!(
+        "
+mprs help:
+    play  => play the current
+    pause => pause the current
+    next  => play the next song
+    prev  => pause the prev song
+    stats => show stats of current song
+    -h | --help | help  => shows this help menu
+"
+    )
 }
